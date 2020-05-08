862ec282f7a2189b37c21f092cbb1f1c61e42976
hongbin.dhb <hongbin.dhb@alibaba-inc.com>
2020-05-08 13:42:24 +0800

    for test

--- python/merge_k_linked_lists.py
+++ python/merge_k_linked_lists.py
@@ -1,23 +1,102 @@
 from typing import List
 from python.common.typing import ListNode
-from python.common.util import printCost, printList
+from python.common.util import printCost
+
+import heapq
 
 
 class Solution:
 
     @printCost
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        ans = ListNode()
+    def mergeKLists3(self, lists: List[ListNode]) -> ListNode:
+        """
+        用堆排序（优先队列）选择最小的元素，生成结果链表
+        :param lists:
+        :return:
+        """
+        # 边界处理
+        if not lists or len(lists) == 0:
+            return None
 
+        heap = []
+        for l in lists:
+            while l:
+                # 构建小顶堆
+                heapq.heappush(heap, l.val)
+                l = l.next
+
+        ans = ListNode()
         node = ans
 
-        return ans
+        while heap:
+            # 从堆中弹出最小元素，构建链表节点
+            node.next = ListNode(heapq.heappop(heap))
+            # 链表向前走
+            node = node.next
+
+        return ans.next
+
+    @printCost
+    def mergeKLists2(self, lists: List[ListNode]) -> ListNode:
+        """
+        依次两两合并法
+        :param lists:
+        :return:
+        """
+        ans = ListNode()
+
+        for l in lists:
+            ans = self.mergeTwoLists(ans, l)
+
+        return ans.next
+
+    @printCost
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        """
+        分治法两两合并
+        :param lists:
+        :return:
+        """
+        # 边界情况
+        if len(lists) == 0:
+            return None
+        elif len(lists) == 1:
+            return lists[0]
+
+        # 寻找中值
+        mid = len(lists) // 2
+
+        # 递归调用 mergeKLists
+        return self.mergeTwoLists(self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:]))
 
     @printCost
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        for x in l1:
-            print(x.val)
-        pass
+        """
+        合并2个有序链表
+        :param l1:
+        :param l2:
+        :return:
+        """
+        # 边界处理
+        if not l1 or not l2:
+            return l1 if l1 else l2
+
+        ans = ListNode()
+        node = ans
+
+        a, b = l1, l2
+        while a and b:
+            if a.val < b.val:
+                node.next = a
+                a = a.next
+            else:
+                node.next = b
+                b = b.next
+            node = node.next
+
+        node.next = a if a else b
+
+        return ans.next
 
 
 """
@@ -34,9 +113,9 @@ s1 = [ListNode(1, ListNode(4, ListNode(5))),
       ListNode(2, ListNode(6))]
 s = [s1]
 
-# for p in s:
-#     print(Solution().mergeKLists(p))
+for p in s:
+    print(Solution().mergeKLists(p))
 
-l1 = ListNode(1, ListNode(4, ListNode(5)))
-l2 = ListNode(1, ListNode(3, ListNode(4)))
-print(Solution().mergeTwoLists(l1, l2))
+# l1 = ListNode(1, ListNode(4, ListNode(5)))
+# l2 = ListNode(1, ListNode(3, ListNode(4)))
+# print(Solution().mergeTwoLists(l1, l2))
